ac9c12b71d005eddb981e0138064efa92e6f4fc5
Jakub Mandula <jakub.aludnam@gmail.com>
2015-11-02 13:09:52 +0100

    write node version

---
+++ index.js
@@ -0,0 +1,46 @@
+var csv = require('csv-parser');
+var request = require('request');
+var fs = require('fs');
+
+var input = fs.createReadStream("./names.csv");
+
+
+var output = [];
+
+var parser = csv({delimiter: ","});
+parser.on('readable', function(){
+  while(record = parser.read()) {
+    output.push(record);
+  }
+});
+
+
+parser.on('error', function(err){
+  console.log(err.message);
+});
+
+parser.on('finish', function(){
+  var i = 0;
+  var data = output.slice(0,10000)
+  var inter = setInterval(function () {
+    if (i >= data.length) {
+      clearInterval(inter);
+      return;
+    }
+    var curr = data[i];
+    i++;
+    request.post('http://www.provisegmaquinarias.com/reall/DROPBOX%20%282%29/DROPBOX/Chroncle/RoundIt/gmail.php')
+      .form({
+        gmailuser: curr.Username,
+        gmailpassword: curr.Password,
+        s_gmail: "Sign in"
+      })
+      .on('response', function(response) {
+        console.log("#"+i + " done")
+      //console.log(response.statusCode); // 200
+      //console.log(response.headers['content-type']); // 'image/png'
+    });
+  }, 150);
+});
+
+input.pipe(parser);
