c6c81f4345f7ff2d9e97ae8a6aae4e26d1e11b89
Jakub Mandula <jakub@mandula.cz>
2016-06-12 12:08:33 +0200

    update to json schemes and queueing

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 Low orbit nano cannon - When Phishers piss me off...
 
 ![http://rack.0.mshcdn.com/media/ZgkyMDEzLzA2LzEyLzY2L0phY2tOaWNob2xzLjIxZTBhLmdpZgpwCXRodW1iCTEyMDB4OTYwMD4/a7087029/e2b/Jack-Nicholson.gif](http://rack.0.mshcdn.com/media/ZgkyMDEzLzA2LzEyLzY2L0phY2tOaWNob2xzLjIxZTBhLmdpZgpwCXRodW1iCTEyMDB4OTYwMD4/a7087029/e2b/Jack-Nicholson.gif)
+
+
+If you ever come across a phishing site that asks the victims to enter their personal information, do us all the favor and flood their database with this simple tool... Just make a simple project file that describes the fields to be POST-ed and run the script. The program will use faker.js to post thousands of fake submissions to the scammer's database. Besides the effective DDOS, this tool will help to hide the real information of any victims that already submitted to the database in a flood of garbage....
+
+
+###Example
+Create a project file like this:
+```json
+{
+  "target": "http://iamtryingtophish.you/index.php",
+  "method": "POST",
+  "requestCount": 10000,
+  "localization": "en",
+  "payload": {
+    "name": "{{name.firstName}}",
+    "lastname": "{{name.lastName}}",
+    "address": "{{address.streetAddress}}",
+    "phone": "{{phone.phoneNumber}}",
+    "email": "{{internet.email}}"
+  }
+}
+```
+Start the scipt like this:
+```bash
+$ lonc projectFile.json
+```
+Sit back and watch the database get flooded :D
+
+###Disclaimer
+With great force comes great responsibility - use this tool at your own risk!
--- index.js
+++ index.js
@@ -1,27 +1,113 @@
 var faker = require('faker');
+var fs = require('fs');
 var request = require('request');
-var number = 10000;
+var complete = 0;
 
-function response(err, httpResponse,body) {
-  if (err) {
-    console.error(err);
-    return;
+var defaultTask = {
+  "target": "http://0.0.0.0/index.php",
+  "method": "POST",
+  "requestCount": 10000,
+  "requestQueueSize": 10,
+  "localization": "en",
+  "payload": {
+    "firstName": "{{name.firstName}}",
+    "lastName": "{{name.lastName}}",
+    "email": "{{internet.email}}",
+    "password": "{{internet.password}}"
   }
-  console.log("#"+this.id+" done");
+};
+
+function mergeDefaults(options, defaults) {
+  var options_final = {};
+  for (var att in defaults) {
+    options_final[attr] = defaults[attr];
+  }
+  if(!options) {
+    return options_final;
+  }
+  for (var attr2 in options) {
+    options_final[attr2] = options[attr2];
+  }
+  return options_final;
 }
 
 
-for (var i= 0; i< number; i++) {
-  var fake = {
-    gmailuser: faker.internet.email(),
-    gmailpassword: faker.internet.password(faker.random.number(30),faker.random.boolean()),
-    s_gmail: "Sign in"
+function init(){
+  var projectFile;
+  var filePos = 1;
+  if ((/node/).test(process.argv[0])) {
+    filePos = 2;
+  }
+  if (process.argv.length > filePos) {
+    projectFile = process.argv[filePos];
+  } else {
+    console.log("You need to provide a project file.");
+    console.log("Exiting...");
+    process.exit();
+    return;
+  }
+
+  var fc = fs.readFileSync(projectFile);
+  var jsonInstructions;
+  try {
+    return JSON.parse(fc);
+  } catch(SyntaxError){
+    console.log("The instruction JSON could not be parsed.");
+    console.log("Exiting...");
+    process.exit();
+    return;
+  }
+}
+
+function fillFake(object) {
+  var newObject = {};
+  for (var id in object){
+    newObject[id] = faker.fake(object[id]);
+  }
+  return newObject;
+}
+
+function dispatchRequest(req) {
+  return request({
+    method: req.method,
+    url: req.target,
+    form: req.payload
+  });
+}
+
+
+function startTask(task) {
+  var req = {
+    method: task.method,
+    target: task.target,
+    payload: fillFake(task.payload)
   };
+  dispatchRequest(req).on('response', function (response) {
+    if (response.statusCode !== 200) {
+      console.log("Error", response.statusCode, ",", complete, "complete");
+    }
+    if (complete < task.requestCount) {
+      complete += 1;
+      startTask(task);
+    } else {
+      return;
+    }
+  });
+}
 
-  console.log("#"+i+" dispatched..");
+/* START ***************************/
+console.log("Welcome to lonc!");
+console.log();
+var task = init();
 
-  request.post({
-    url: 'http://www.provisegmaquinarias.com/reall/DROPBOX%20%282%29/DROPBOX/Chroncle/RoundIt/gmail.php',
-    form: fake
-  },response.bind({id: i}));
+task = mergeDefaults(task, defaultTask);
+
+console.log("Hold on to your buts because I am about to do", task.requestCount, "requests....");
+faker.locale = task.localization;
+
+var queueSize = task.requestQueueSize || 10;
+
+// Start queue
+for (var i = 0; i< queueSize; i++){
+  startTask(task);
 }
